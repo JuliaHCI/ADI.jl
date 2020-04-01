b7bfe69e079409773544fd6ff95c6b4ef34a6ebe
Miles Lucas <mdlucas@hawaii.edu>
2020-04-01 13:19:40 -1000

    update ci

--- src/pca.jl
+++ src/pca.jl
@@ -2,26 +2,6 @@
     pca(cube, angles; ncomps=size(cube, 1), pratio=0.99)
 
 Use principal component analysis (PCA) to reduce data cube.
-
-# Examples
-
-```jldoctest
-julia> cube = ones(30, 100, 100);
-
-julia> d = design(ADI.PCA, cube);
-
-julia> d.info
-Dict{Symbol,Any} with 2 entries:
-  :ncomps => 30
-  :pratio => 1.0
-
-julia> d = design(ADI.PCA, cube, ncomps=5);
-
-julia> d.info
-Dict{Symbol,Any} with 2 entries:
-  :ncomps => 5
-  :pratio => 1.0
-```
 """
 function pca(cube::AbstractArray, angles::AbstractVector; ncomps = size(cube, 1), pratio = 0.99)
     # transform cube
@@ -35,3 +15,24 @@ function pca(cube::AbstractArray, angles::AbstractVector; ncomps = size(cube, 1)
 
     return combine(derotate!(res, angles), method = median)
 end
+
+# """
+#     pca(cube, angles; ncomps=size(cube, 1), pratio=0.99)
+
+# Use principal component analysis (PCA) to reduce data cube.
+# """
+# function pca(cube::AbstractArray, angles::AbstractVector, ref::AbstractArray; ncomps = size(cube, 1), pratio = 0.99)
+#     # transform cubes
+#     rf, ry, rx = size(ref)
+#     X = reshape(ref, rf, ry * rx)
+#     nf, ny, nx = size(cube)
+#     Y = reshape(cube, nf, ny * nx)
+    
+#     # Fit to our reference, X, and reconstruct
+#     P = mvs.fit(mvs.PCA, X; maxoutdim = ncomps, pratio = pratio, mean = 0)
+#     S = mvs.reconstruct(P, mvs.transform(P, Y))
+    
+#     res = cube .- reshape(S, nf, ny, nx)
+
+#     return combine(derotate!(res, angles), method = median)
+# end
--- test/pca.jl
+++ test/pca.jl
@@ -8,4 +8,14 @@
         @test size(res) == (512, 512)
         @test res ≈ ones(512, 512)
     end
+
+    @testset "RDI Trivial" begin
+        data = randn(30, 512, 512)
+        angles = 90 .* rand(30)
+        
+        @test pca(data, angles, zeros(20, 512, 512)) ≈ data
+
+        # interface
+        @test @inferred(pca(data, angles)) == @inferred(pca(data, angles, data))
+    end
 end
