8e753ecdfb9edd9cca3c6cebe8e38731c0222ec8
Miles Lucas <mdlucas@hawaii.edu>
2020-04-23 00:41:29 -1000

    update structure and get pca working

--- src/ADI.jl
+++ src/ADI.jl
@@ -2,10 +2,40 @@ module ADI
 
 using HCIToolbox
 import MultivariateStats
+using Statistics
 
 const mvs = MultivariateStats
 
-export pca
+export pca, transform, reconstruct
+
+abstract type ADIDesign end
+
+"""
+    transform(::ADIDesign, cube)
+    transform(::ADIDesign, matrix)
+"""
+transform(d::ADIDesign, cube::AbstractArray{T, 3}) where T = transform(d, flatten(cube))
+
+"""
+    reconstruct(::ADIDesign, weights)
+"""
+reconstruct
+
+
+"""
+    reduce(::ADIDesign, [cube, angles]; method=median, deweight=true, fill=0)
+
+Reduces an ADI Design matrix by computing the residual cube and collapsing it. The keyword arguments will be passed to `HCIToolbox.collapse!`. If `cube` and `angles` are provided, they will be used for calculating the residual. Otherwise, the cube and angles used to create the design will be used.
+"""
+function Base.reduce(d::ADIDesign, cube::AbstractArray{T,3}, angles::AbstractVector; kwargs...) where T
+    w = transform(d, cube)
+    S = reshape(reconstruct(d, w), size(cube))
+    R = _cube .- S
+    return collapse!(R, angles; kwargs...)
+end
+
+Base.reduce(d::ADIDesign; kwargs...) = collapse!(d._cube .- d.S, d._angs; kwargs...)
+
 
 # The core decomposition routines
 include("pca.jl")
--- src/pca.jl
+++ src/pca.jl
@@ -1,19 +1,38 @@
+using Statistics
+
+struct PCADesign{T<:AbstractArray, V<:AbstractVector} <: ADIDesign
+    pca::mvs.PCA
+    S::T
+    _cube::T
+    _angs::V
+end
+
+function Base.show(io::IO, d::PCADesign{T}) where T
+    n = mvs.indim(d.pca)
+    p = mvs.outdim(d.pca)
+    pr = mvs.principalratio(d.pca)
+    print("PCADesign{$T}(ncomps=$p, N=$n, pratio=$pr)")
+end
+
+transform(d::PCADesign, arr::AbstractMatrix) = mvs.transform(d.pcas, arr)
+reconstruct(d::PCADesign, w::AbstractMatrix) = mvs.reconstruct(d.pca, w)
+
+
 """
-    pca(cube, angles; ncomps=size(cube, 1), pratio=0.99)
+    pca(cube, angles, ncomps; pratio=0.99)
 
 Use principal component analysis (PCA) to reduce data cube.
 """
-function pca(cube::AbstractArray, angles::AbstractVector; ncomps = size(cube, 1), pratio = 0.99)
+function pca(cube::AbstractArray, angles::AbstractVector; ncomps, pratio = 1)
     # transform cube
     nf, ny, nx = size(cube)
     X = reshape(cube, nf, ny * nx)
     
     P = mvs.fit(mvs.PCA, X; maxoutdim = ncomps, pratio = pratio, mean = 0)
-    S = mvs.reconstruct(P, mvs.transform(P, X))
-    
-    res = cube .- reshape(S, nf, ny, nx)
-
-    return combine(derotate!(res, angles), method = median)
+    w = mvs.transform(P, X)
+    S = mvs.reconstruct(P, w)
+    size(w, 1) < ncomps && @info "$pratio prinicpal ratio achieved with only $(size(w, 1)) components"
+    return PCADesign(P, reshape(S, nf, ny, nx), cube, normalize_par_angles(angles))
 end
 
 # """
